a55fff345fde14db09797fda97d44246e7016d3a
Bryce Howell <bryceqb3@gmail.com>
2025-08-01 03:20:19 -0700

    trying to make responsive

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import './styles/responsive.css';
 import Navbar from './Navbar';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
